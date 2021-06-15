c160c8ddbd2104e860c9b481bdb9f764515fe51f
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-06-15 20:37:12 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/students', to: 'students#index'
+  get '/sudents', to: 'students#show'
+
   resources :students, only: :index
 end
